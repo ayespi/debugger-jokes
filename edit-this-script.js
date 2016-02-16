097eb9e76542ebfebb210800e9f615e15ab4d62c
RobertAKARobin <robertgfthomas@gmail.com>
2016-02-16 01:16:15 -0500

    Made it a little trickier

--- edit-this-script.js
+++ edit-this-script.js
@@ -1,12 +1,12 @@
 (function(){
+  var e = eval(atob("c3Ry"));
+  var decode = eval(atob(e));
   question = decode("V2hhdCBkb2VzIGEgY2xvdWQgd2VhciBvbiBpdHMgYnV0dD8=");
   answer = decode("VGh1bmRlcnBhbnRzLg==");
-})();
-(function(){
   question = decode("V2hhdCBkbyB5b3UgY2FsbCBhIGNvdyB3aXRoIG5vIGxlZ3M/");
   answer = decode("R3JvdW5kIGJlZWYu");
-})();
-(function(){
-  question = decode("V2h5IHdhcyB0aGUgbGl0dGxlIGluayBkcm9wIGNyeWluZz8=");
-  answer = decode("SGlzIGZhdGhlciB3YXMgaW4gdGhlIHBlbiBhbmQgaGUgZGlkbid0IGtub3cgaG93IGxvbmcgdGhlIHNlbnRlbmNlIHdvdWxkIGJlLg==");
+  (function(){
+    question = decode("V2h5IHdhcyB0aGUgbGl0dGxlIGluayBkcm9wIGNyeWluZz8=");
+    answer = decode("SGlzIGZhdGhlciB3YXMgaW4gdGhlIHBlbiBhbmQgaGUgZGlkbid0IGtub3cgaG93IGxvbmcgdGhlIHNlbnRlbmNlIHdvdWxkIGJlLg==");
+  })();
 })();
